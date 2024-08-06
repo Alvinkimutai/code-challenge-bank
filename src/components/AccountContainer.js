b23235ba95a7f05d8aced5d206e6eb4c0cec9d22
Alvinkimutai <alvinchepkiyeng@gmail.com>
2024-08-06 22:52:03 +0300

    implements code deliverables

--- src/components/AccountContainer.js
+++ src/components/AccountContainer.js
@@ -1,14 +1,55 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import TransactionsList from "./TransactionsList";
 import Search from "./Search";
 import AddTransactionForm from "./AddTransactionForm";
 
 function AccountContainer() {
+  const [transactions, setTransactions] = useState([]);
+  const [userSearch, setUserSearch] = useState('');
+
+  useEffect(() => {
+    fetch("http://localhost:8001/transactions")
+      .then((response) => response.json())
+      .then((data) => setTransactions(data))
+      .catch((error) => console.error(error));
+  }, []);
+
+  function handleNewTransaction(addNewTransaction) {
+    fetch("http://localhost:8001/transactions", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(addNewTransaction),
+    })
+      .then((res) => {
+        if (res.ok) {
+          return res.json(); 
+        }
+        throw new Error("Failed to add transaction");
+      })
+      .then((newTransaction) => {
+        setTransactions((prevState) => [...prevState, newTransaction]);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  }
+
+  function searchFn(event) {
+    setUserSearch(event.target.value);
+  }
+
+  
+  const searchFilter = transactions.filter((transaction) =>
+    transaction.description.toLowerCase().includes(userSearch.toLowerCase())
+  );
+
   return (
     <div>
-      <Search />
-      <AddTransactionForm />
-      <TransactionsList />
+      <Search onChange={searchFn} /> {}
+      <AddTransactionForm submitForm={handleNewTransaction} />
+      <TransactionsList transactions={searchFilter} />
     </div>
   );
 }
--- src/components/AddTransactionForm.js
+++ src/components/AddTransactionForm.js
@@ -1,14 +1,44 @@
-import React from "react";
+import React, { useState } from "react";
 
-function AddTransactionForm() {
+
+function AddTransactionForm({submitForm}) {                   
+  const [formData, setFormData] = useState({
+      date: "",
+      description:"",
+      category:"",
+      amount:""
+  })
+
+  function handleChange (event){
+      let formInput = {...formData, [event.target.name]:event.target.value};
+      setFormData(formInput)
+    
+
+  }
+  function handleSubmit(event){
+    event.preventDefault()
+    if (!formData.date || !formData.description || !formData.category || formData.amount <= 0) {
+      alert("Please fill in all fields with valid data.");
+      return;
+    }
+    submitForm(formData)
+      setFormData({
+        date: "",
+        description: "",
+        category: "",
+        amount: ""
+    })
+  }
+
+
   return (
     <div className="ui segment">
-      <form className="ui form">
+      <form className="ui form" onChange={handleChange} onSubmit={handleSubmit}> 
         <div className="inline fields">
-          <input type="date" name="date" />
-          <input type="text" name="description" placeholder="Description" />
-          <input type="text" name="category" placeholder="Category" />
-          <input type="number" name="amount" placeholder="Amount" step="0.01" />
+          <input type="date" name="date"value={formData.date} onChange={handleChange} />
+          <input type="text" name="description" placeholder="Description" value={formData.description} onChange={handleChange}/>
+          <input type="text" name="category" placeholder="Category" value={formData.category} onChange={handleChange} />
+          <input type="number" name="amount" placeholder="Amount" step="0.01" value={formData.amount} onChange={handleChange} />
         </div>
         <button className="ui button" type="submit">
           Add Transaction
